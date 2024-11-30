b2dec663d4ba88f12fd8121e767f4fa7af9a8092
Rezkyindri31 <147515669+Rezkyindri31@users.noreply.github.com>
2024-11-30 12:51:05 +0700

    Update InformasiGeofisika.js

--- app/Produk/components/InformasiGeofisika.js
+++ app/Produk/components/InformasiGeofisika.js
@@ -103,6 +103,8 @@ export default function InformasiGeofisika() {
                   {new Intl.NumberFormat("id-ID", {
                     style: "currency",
                     currency: "IDR",
+                    minimumFractionDigits: 0, 
+                    maximumFractionDigits: 0,
                   }).format(product.Harga)}
                 </p>
                 <Button
